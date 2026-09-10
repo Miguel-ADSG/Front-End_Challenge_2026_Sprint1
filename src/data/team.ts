8fd56d66228f4453948bb7ba32f63b6be2b78dad
Miguel-V-M-dev <martins.miguelvieira@gmail.com>
2026-09-10 08:45:59 -0300

    feat: adicionando dados e fotos da equipe

---
+++ src/data/team.ts
@@ -0,0 +1,54 @@
+import carlos from "../assets/img/Carlos.jpeg";
+import gustavo from "../assets/img/Gustavo.jpeg";
+import miguel from "../assets/img/Miguel.jpeg";
+import murilo from "../assets/img/Murilo.jpeg";
+import thiago from "../assets/img/Thiago.jpeg";
+import type { TeamMember } from "../types";
+
+export const team: TeamMember[] = [
+  {
+    id: 1,
+    nome: "Miguel Vieira Martins",
+    rm: "571978",
+    turma: "1-TDSPG",
+    foto: miguel,
+    github: "https://github.com/Miguel-ADSG",
+    linkedin: "https://www.linkedin.com/in/miguel-vieira-martins-598964406",
+  },
+  {
+    id: 2,
+    nome: "Gustavo Neri Andrade",
+    rm: "572722",
+    turma: "1-TDSPG",
+    foto: gustavo,
+    github: "https://github.com/GustavoNeriAndrade",
+    linkedin: "https://www.linkedin.com/in/gustavo-neri-andrade-163795337",
+  },
+  {
+    id: 3,
+    nome: "Thiago Vendrami Luca",
+    rm: "572942",
+    turma: "1-TDSPG",
+    foto: thiago,
+    github: "https://github.com/Thiagovluca",
+    linkedin: "https://br.linkedin.com/in/thiago-vendrami-luca-4892a6409",
+  },
+  {
+    id: 4,
+    nome: "Carlos Americo Machado Brambilla",
+    rm: "571250",
+    turma: "1-TDSPG",
+    foto: carlos,
+    github: "https://github.com/CarlosAmericoMachado",
+    linkedin: "https://www.linkedin.com/in/carlos-brambilla-b5b971407",
+  },
+  {
+    id: 5,
+    nome: "Murilo da Silva Lourenço",
+    rm: "573959",
+    turma: "1-TDSPG",
+    foto: murilo,
+    github: "https://github.com/murilojosh",
+    linkedin: "https://www.linkedin.com/in/murilo-lourenço-35250b411",
+  },
+];
